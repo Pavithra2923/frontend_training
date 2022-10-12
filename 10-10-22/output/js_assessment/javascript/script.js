bc89e374ba1e80583f700d0167a9d3e37ef10250
Pavithra2923 <pavithra2972001@gmail.com>
2022-10-12 17:09:32 +0530

    js project

--- 10-10-22/output/js_assessment/javascript/script.js
+++ 10-10-22/output/js_assessment/javascript/script.js
@@ -1,4 +1,4 @@
-// add hotel details
+//  add hotel details
 var hotel_details=[{
     "id":1,
     "hotel_name":"Arya Bhavan",
@@ -6,7 +6,7 @@ var hotel_details=[{
     "location":"Vadasery"},
     {
     "id":2,
-    "hotel_name":"KFC",
+    "hotel_name":"Dominos",
     "categories":["Vegiterian","Non-Vegiterian"],
     "location":"Nagercoil"
     },
@@ -17,21 +17,71 @@ var hotel_details=[{
     "location":"Nagercoil"
     }
 ]
-// store id value to localstorage
-let id_val = [];
-function test() {
-    for ( i = 0; i <hotel_details.length; i++){
-       id_val.push(hotel_details[i].id);
+// SCRIPT FOR STORE SIGNUP DETAILS IN LOCAL STORAGE
+  if(!localStorage.getItem("user")){
+    localStorage.setItem("user",JSON.stringify([]));
+}
+const list=JSON.parse(localStorage.getItem("user"));
+function addElement(){
+    const details={
+        name:document.getElementById('sname').value,
+        email:document.getElementById('smail').value,
+        pwd:document.getElementById('spwd').value,
+        srepeat:document.getElementById('srepeat').value,
+         };
+    list.push(details);
+    localStorage.setItem("user",JSON.stringify(list))
+    alert("Successfully Registered")
+    
+ }
+// LOGIN SCRIPT
+ 
+ function loginUser(){
+    let loginEmail=document.getElementById('login-email').value;
+    let loginPass=document.getElementById('login-pass').value;
+  if(localStorage.getItem('user')){
+     let login_user=JSON.parse(localStorage.getItem('user'));
+     for(let i=0;i<login_user.length;i++){
+        if(loginEmail === login_user[i].email && loginPass ===login_user[i].pwd) {
+            window.location.replace("home.html"); 
+            break;
+        }else{
+            alert("please username and password check");
+            break;
+        }
     }
-    console.log(id_val)
+    }else{
+        console.log('Not a registered user')
+    }
+ }
+ 
+// store id value to localstorage
+ let id_val =[];
+ function test() {
+     for ( i = 0; i <hotel_details.length; i++){
+        id_val.push(hotel_details[i].id);
+     }
+     console.log(id_val)
+ }
+ test();
+ function local_display(){
+   if(!localStorage.getItem("hotel_id")){
+   localStorage.setItem("hotel_id",JSON.stringify([]))
+   }
+   let hotel_list=JSON.parse(localStorage.getItem("hotel_id"))
+   hotel_list.push(id_val)
+   localStorage.setItem("hotel_id",JSON.stringify(hotel_list));
+ }local_display()
+
+// VALUES CALL BY ID
+
+function getid(){
+    let element=""
+    let list_id=JSON.parse(localStorage.getItem("hotel_id"))
+    console.log(list_id)
+    for(let i=0;i<list_id.length;i++){
+        console.log(i)
 }
-test();
-function local_display(){
-  if(!localStorage.getItem("hotel_id")){
-  localStorage.setItem("hotel_id",JSON.stringify([]))
-  }
-  let hotel_list=JSON.parse(localStorage.getItem("hotel_id"))
-  hotel_list.push(id_val)
-  localStorage.setItem("hotel_id",JSON.stringify(hotel_list));
-}
-local_display();
+}getid()
+
+  
