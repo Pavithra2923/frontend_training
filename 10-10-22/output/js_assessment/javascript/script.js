ba25047fed49142d3a29ba7189c015b63c89c48b
Pavithra2923 <pavithra2972001@gmail.com>
2022-10-13 17:10:05 +0530

    js project

--- 10-10-22/output/js_assessment/javascript/script.js
+++ 10-10-22/output/js_assessment/javascript/script.js
@@ -1,24 +1,252 @@
 //  add hotel details
-var hotel_details=[{
-    "id":1,
-    "hotel_name":"Arya Bhavan",
-    "categories":"Vegiterian",
-    "location":"Vadasery"},
-    {
-    "id":2,
-    "hotel_name":"Dominos",
-    "categories":["Vegiterian","Non-Vegiterian"],
-    "location":"Nagercoil"
-    },
-    {
-    "id":3,
-    "hotel_name":"Odel",
-    "categories":["Vegiterian","Non-Vegiterian"],
-    "location":"Nagercoil"
+var hotel_details={
+
+//  fst hotel starts here
+    aryabhavan:[{
+        "id":1,
+        "hotel_name":"Arya Bhavan",
+        "categories":"Vegiterian",
+        "location":"Vadasery",
+        "menu":[{
+            "breakfast":[{"title":"minitiffen",
+                        "price":"Rs.100",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToSnUAkx4U98-ZWx62XwcOxQCzAy11o_v6ZQ&usqp=CAU",
+                        "serve_for":2,
+                        "description":"Pure veg,2 mini-idly,min-pongal,mini-dosa,tea"
+                        },
+                        {
+                        "title":"ghee-roast",
+                        "price":"Rs.100",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqMusVeqg1cRnYPMGewYphXJ_mxTnMLmNUhQ&usqp=CAU",
+                        "serve_for":1,
+                        "description":"Pure veg,with 3varity of chutneys"
+                        }],
+
+            "lunch":[{"title":"full_meals",
+                        "price":"Rs.170",
+                        "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/apxoeoqyzisb7wl3jkoe",
+                        "serve_for":2,
+                        "description":"Pure veg,sambar,gravys,curry's,pappad,payasam"
+                    },
+                    {"title":"panner masaala",
+                        "price":"Rs.170",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrWLUMPrhNAZyZm9XdvxFEBhyDyHv2LfFk2g&usqp=CAU",
+                        "serve_for":1,
+                        "description":"Pure veg,panner gravy with little peices"
+                    }],
+            "dinner":[{
+                        "title":"sapathi",
+                        "price":"Rs.60",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTMQjjLfZ1Msfvyf3xMaACjmPN6yxPrQ4focWqcubZG_5IbX78WcNAHSnfJosN7RXcjIc&usqp=CAU",
+                        "serve_for":1,
+                        "description":"Pure veg,chutney,veg kuruma"
+                    },
+                    {
+                        "title":"noodles",
+                        "price":"Rs.180",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY54mMtbNwNfcJp3Zc7TpyT7EA2dvkDzlx4Q&usqp=CAU",
+                        "serve_for":2,
+                        "description":"Pure veg,ketcup,mixed of veggies"
+                    }],
+            "dessert":[{
+                        "title":"vennila milkshake",
+                        "price":"Rs.100",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToILXdGXgNIxnqTArtGJpjVCEKCsKxtcxZdA&usqp=CAU",
+                        "serve_for":1,
+                        "description":"Pure veg,chill,creamy"
+                    },
+                    {
+                        "title":"brownie with icecream",
+                        "price":"Rs.200",
+                        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnJN6Vw5vWiFnlW27uO5z2gAvrpArq1lyL_Q&usqp=CAU",
+                        "serve_for":1,
+                        "description":"Pure veg,chill,creamy"
+                    },
+                    {
+                        "title":"special falooda",
+                        "price":"Rs.200",
+                        "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gejkcayqho3cjyil0dsk",
+                        "serve_for":2,
+                        "description":"Pure veg,chill,creamy ,mixed icream, nuts"
+                    }
+                    ],
+
+                }]
+            }],
+        
+   // add second hotel starts here
+   
+       odel:[{
+            "id":2,
+            "hotel_name":"Odel_Restarent",
+            "categories":["Vegiterian","Non-Vegiterian"],
+            "location":"Nagercoil",
+            "menu":[{
+                "lunch":[{"title":"full_meals",
+                            "price":"Rs.150",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/apxoeoqyzisb7wl3jkoe",
+                            "serve_for":2,
+                            "description":"Pure veg,sambar,gravys,curry's,pappad,payasam"
+                        },
+                        {"title":"chicken briyani",
+                            "price":"Rs.210",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMh5zbgs8lxJDF_RI2BuYbT1GtYDFx6hYiyQ&usqp=CAU",
+                            "serve_for":2,
+                            "description":"Non-veg,onion raitha,brinjal gravy,1egg,2chicken peices"
+                        },
+                        {"title":"mutton sukka",
+                            "price":"Rs.370",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpfUN409nhOOYXX6u-3wdWHTyvqayhU3HxRw&usqp=CAU",
+                            "serve_for":2,
+                            "description":"Non-veg,onion raitha,brinjal gravy,1egg,2chicken peices"
+                            
+                        }
+                    ],
+                "dinner":[{
+                            "title":"protta with beef gravy",
+                            "price":"Rs.230",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_tCQzMfriidsdOx5xPmi13DZuJ6i7AH2kgQ&usqp=CAU",
+                            "serve_for":2,
+                            "description":"non-veg,3protta,beef gravy,little peice of onions"
+                        },
+                        {
+                            "title":"noodles",
+                            "price":"Rs.180",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY54mMtbNwNfcJp3Zc7TpyT7EA2dvkDzlx4Q&usqp=CAU",
+                            "serve_for":2,
+                            "description":"Pure veg,ketcup,mixed of veggies"
+                        },
+                        {
+                            "title":"tandoori chicken",
+                            "price":"Rs.420",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8M7oivXWPeUA1-6QOSF42g6_khEoRFBPDmg&usqp=CAU",
+                            "serve_for":4,
+                            "description":"non-veg,mayo,pickled veggies"
+                        }],
+                "dessert":[{
+                            "title":"milkshake",
+                            "price":"Rs.100",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToILXdGXgNIxnqTArtGJpjVCEKCsKxtcxZdA&usqp=CAU",
+                            "serve_for":1,
+                            "description":"Pure veg,chill,creamy"
+                        },
+                        {
+                            "title":"brownie with icecream",
+                            "price":"Rs.200",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnJN6Vw5vWiFnlW27uO5z2gAvrpArq1lyL_Q&usqp=CAU",
+                            "serve_for":1,
+                            "description":"Pure veg,chill,creamy"
+                        },
+                        {
+                            "title":"special falooda",
+                            "price":"Rs.200",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gejkcayqho3cjyil0dsk",
+                            "serve_for":2,
+                            "description":"Pure veg,chill,creamy ,mixed icream, nuts"
+                        }],
+    
+                    }]
+                }],
+
+    // third hotel added list
+
+    dominos:[{
+            "id":3,
+            "hotel_name":"Domino's",
+            "categories":["Vegiterian","Non-Vegiterian"],
+            "location":"Nagercoil",
+            "menu":[{
+                "pizza":[{
+                            "title":"cheesy corn",
+                            "price":"Rs.150",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ukiq6hopfqwmem48yjj3",
+                            "serve_for":2,
+                            "description":"Pure veg,corn,cheese"
+                         },
+                         {
+                            "title":"tandoori paneer ",
+                            "price":"Rs.210",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gh7ofyylkvv6ifpih65y",
+                            "serve_for":2,
+                            "description":"Pure veg,paneer,bell peppers,cheese"
+                         },
+                         {
+                            "title":"Spiced Double Chicken",
+                            "price":"Rs.350",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/ntw7wftfyl1uzdd9u5bf",
+                            "serve_for":2,
+                            "description":"A classic American taste! Relish the delectable flavor of Chicken Pepperoni, topped with extra cheese"
+                         },
+                         {
+                            "title":"Chicken Pepperoni",
+                            "price":"Rs.400",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/xjjoonwqop3ojyyfj4u5",
+                            "serve_for":2,
+                            "description":"A classic American taste! Relish the delectable flavor of Chicken Pepperoni, topped with extra cheese"
+                         } ],
+                "fries":[{
+                            "title":"Roasted Chicken Wings Peri-Peri",
+                            "price":"Rs.200",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/nvocia0tbadvv8forqne",
+                            "serve_for":2,
+                            "description":"Crisp, roasted chicken wings in classic hot sauce and peri-peri flavour"
+                         },
+                         {
+                            "title":"tandoori chicken",
+                            "price":"Rs.300",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/nvocia0tbadvv8forqne",
+                            "serve_for":2,
+                            "description":"Crisp, roasted chicken wings in classic hot sauce and peri-peri flavour"      
+                         },
+                         {
+                            "title":"Creamy Tomato Pasta Veg",
+                            "price":"Rs.120",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/vyjjd29lce6zdpqlx4k9",
+                            "serve_for":1,
+                            "description":"Instant Fusilli Pasta, Harissa Sauce, Onion, Mushroom, Olives, Parsley sprinkle"
+                         }],
+             "dessert":[{
+                            "title":"milkshake",
+                            "price":"Rs.100",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToILXdGXgNIxnqTArtGJpjVCEKCsKxtcxZdA&usqp=CAU",
+                            "serve_for":1,
+                            "description":"Pure veg,chill,creamy"
+                        },
+                        {
+                            "title":"brownie with icecream",
+                            "price":"Rs.200",
+                            "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnJN6Vw5vWiFnlW27uO5z2gAvrpArq1lyL_Q&usqp=CAU",
+                            "serve_for":1,
+                            "description":"Pure veg,chill,creamy"
+                        },
+                        {
+                            "title":"special falooda",
+                            "price":"Rs.200",
+                            "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/gejkcayqho3cjyil0dsk",
+                            "serve_for":2,
+                            "description":"Pure veg,chill,creamy ,mixed icream, nuts"
+                        }]
+            }]
+            }]
+    
+ }
+
+// ADD PRODUCTS TO LOCAL STORAGE 
+   
+function product_display(){
+        if(!localStorage.getItem("product_detail")){
+            localStorage.setItem('product_detail',JSON.stringify([]));
+            let product_list=JSON.parse(localStorage.getItem("product_detail"));
+            product_list.push(hotel_details)
+            localStorage.setItem("product_detail",JSON.stringify(product_list));
+        } 
     }
-]
+    product_display()
+    
+
 // SCRIPT FOR STORE SIGNUP DETAILS IN LOCAL STORAGE
-  if(!localStorage.getItem("user")){
+ 
+if(!localStorage.getItem("user")){
     localStorage.setItem("user",JSON.stringify([]));
 }
 const list=JSON.parse(localStorage.getItem("user"));
@@ -49,39 +277,36 @@ function addElement(){
             alert("please username and password check");
             break;
         }
-    }
-    }else{
-        console.log('Not a registered user')
-    }
+    }}
  }
  
 // store id value to localstorage
- let id_val =[];
- function test() {
-     for ( i = 0; i <hotel_details.length; i++){
-        id_val.push(hotel_details[i].id);
-     }
-     console.log(id_val)
- }
- test();
- function local_display(){
+ 
+function local_display(){
    if(!localStorage.getItem("hotel_id")){
-   localStorage.setItem("hotel_id",JSON.stringify([]))
+    localStorage.setItem("hotel_id",JSON.stringify([]));
+    let id_val=JSON.parse(localStorage.getItem("hotel_id"));
+    for ( i = 0; i <hotel_details.length; i++){
+        id_val.push(hotel_details[i].id);
+        console.log(id_val)
+    }
+    localStorage.setItem("hotel_id",JSON.stringify(id_val));
    }
-   let hotel_list=JSON.parse(localStorage.getItem("hotel_id"))
-   hotel_list.push(id_val)
-   localStorage.setItem("hotel_id",JSON.stringify(hotel_list));
- }local_display()
+ }
+ local_display()
 
-// VALUES CALL BY ID
+//  VALUES CALL BY ID
 
 function getid(){
-    let element=""
-    let list_id=JSON.parse(localStorage.getItem("hotel_id"))
-    console.log(list_id)
-    for(let i=0;i<list_id.length;i++){
-        console.log(i)
+        let element="";
+        let list_id=JSON.parse(localStorage.getItem("hotel_id"));
+        console.log(list_id)
+        for(let i=0;i<list_id.length;i++){
+            element=element+" <a href='id.html?id="+list_id[i].id +"</a>"
+            alert(list_id[i].id);
+        }
+
 }
-}getid()
+
 
   
